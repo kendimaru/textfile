51af3fbab2712ab88e6b0862715ac101822fa556
kenjimaru <kendimaru2@gmail.com>
2021-06-26 13:30:32 +0900

    Implement read() function.

--- tests/test_textfile.py
+++ tests/test_textfile.py
@@ -1,44 +1,108 @@
 from textfile import __version__
 import textfile
+from pytest import fixture
+import pytest
 
 
-KENJIMARU = 'kenjimaru'
+KENJIMARU = 'kenjimaru😃'
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.1.1'
+
+
+@fixture
+def tmp_file(tmp_path):
+    return tmp_path / 'tmp.txt'
 
 
 class TestWriteFunction:
+    def test_pathlib_path(self, tmp_file):
+        textfile.write(tmp_file, KENJIMARU)
 
-    def test_pathlib_path(self, tmp_path):
-        p = tmp_path / 'tmp.txt'
-        textfile.write(p, KENJIMARU)
-
-        with open(p, encoding='utf-8') as f:
-            written = f.read()
+        with open(tmp_file, encoding='utf-8') as reader:
+            written = reader.read()
 
         assert written == KENJIMARU
 
-    def test_path_string(self, tmp_path):
-        p = tmp_path / 'tmp.txt'
-        textfile.write(str(p), KENJIMARU)
+    def test_path_string(self, tmp_file):
+        textfile.write(str(tmp_file), KENJIMARU)
 
-        with open(p, encoding='utf-8') as f:
-            written = f.read()
+        with open(tmp_file, encoding='utf-8') as reader:
+            written = reader.read()
 
         assert written == KENJIMARU
 
-    def test_write_empty_string_should_only_create_file(self):
-        raise NotImplemented()
+    def test_set_file_parameter_to_none_should_type_error(self):
+        with pytest.raises(TypeError):
+            textfile.write(None, '')
 
-    def test_write_empty_string_should_erase_content_of_already_existing_file(self):
-        raise NotImplemented()
+    def test_set_s_parameter_to_none_should_type_error(self, tmp_file):
+        with pytest.raises(TypeError):
+            textfile.write(tmp_file, None)
 
-    def test_write_to_nonexist_directory_should_file_not_found_error(self):
-        raise NotImplemented()
+    def test_write_empty_string_should_only_create_file(self, tmp_file):
+        textfile.write(tmp_file, '')
+        assert tmp_file.exists()
 
-    def test_create_file_in_not_permitted_dir_should_permission_error(self):
-        raise NotImplemented()
+    def test_write_empty_string_should_erase_content_of_already_existing_file(self, tmp_file):
+        textfile.write(tmp_file, KENJIMARU)
+        textfile.write(tmp_file, '')
+        with open(tmp_file, encoding='utf-8') as reader:
+            assert reader.read() == ''
+
+    def test_write_to_nonexistent_directory_should_file_not_found_error(self, tmp_path):
+        file_in_nonexistent_directory = tmp_path / 'not_exist' / 'tmp.txt'
+
+        with pytest.raises(FileNotFoundError):
+            textfile.write(file_in_nonexistent_directory, KENJIMARU)
+
+    def test_create_file_in_not_permitted_dir_should_permission_error(self, tmp_path):
+        read_only_dir = tmp_path / 'readonly'
+        read_only_dir.mkdir(mode=0o444)
+
+        with pytest.raises(PermissionError):
+            textfile.write(read_only_dir / 'tmp.txt', KENJIMARU)
+
+        print(read_only_dir)
+
+    def test_write_target_is_directory_should_is_a_directory_error(self, tmp_path):
+        with pytest.raises(IsADirectoryError):
+            textfile.write(tmp_path, KENJIMARU)
+
+
+class TestReadFunction:
+    def test_pathlib_path(self, tmp_file):
+        textfile.write(tmp_file, KENJIMARU)
+        assert textfile.read(tmp_file), KENJIMARU
+
+    def test_path_string(self, tmp_file):
+        textfile.write(tmp_file, KENJIMARU)
+        assert textfile.read(tmp_file.__fspath__(), KENJIMARU)
+
+    def test_set_file_parameter_to_none_should_type_error(self):
+        with pytest.raises(TypeError):
+            textfile.read(None)
+
+    def test_read_from_empty_file(self, tmp_file):
+        textfile.write(tmp_file, '')
+        assert textfile.read(tmp_file) == ''
+
+    def test_read_from_nonexistence_file_should_file_not_found_error(self, tmp_file):
+        with pytest.raises(FileNotFoundError):
+            textfile.read(tmp_file)
+
+    def test_read_from_nonexistence_file_with_silent_true_should_get_empty_string(self, tmp_file):
+        assert textfile.read(tmp_file, silent=True) == ''
+
+    def test_read_from_directory_should_is_a_directory_error(self, tmp_path):
+        with pytest.raises(IsADirectoryError):
+            textfile.read(tmp_path)
+
+    def test_read_from_not_permitted_file_should_permission_error(self, tmp_file):
+        textfile.write(tmp_file, KENJIMARU)
+        tmp_file.chmod(0o222)
+        with pytest.raises(PermissionError):
+            textfile.read(tmp_file)
 
 
--- textfile/__init__.py
+++ textfile/__init__.py
@@ -1,20 +1,22 @@
-__version__ = '0.1.0'
 from pathlib import Path
+
+
+__version__ = '0.1.1'
+_ENCODING = 'utf-8'
 
 
 def write(file, s, overwrite=True):
     """ Open file for write mode, write string into it, and close.
 
-    Write string to a file in encoding ``utf-8``.
+    Write string to a file in ``utf-8`` encoding.
 
     If the file already exists, overwrite that by default,
-    or FileExistsError on overwrite=False.
-
-
+    or FileExistsError if overwrite parameter set to False.
 
     Parameters
     ----------
-    file: str or pathlib.Path
+    file: str or os.PathLike
+        File to write to.
     s: str
         A string to write to file.
     overwrite: bool, default=True
@@ -32,9 +34,13 @@ def write(file, s, overwrite=True):
     FileExistsError:
         If file already exists and overwrite set to False.
     PermissionError
-        If can not open or create file due to permission problem.
+        Could not open or create file due to permission problem.
+    IsADirectoryError
+        A value specified to file parameter was a directory.
+    TypeError
+        Illegal type of parameter specified.
     """
-    with open(file, 'w', encoding='utf-8') as writer:
+    with open(file, 'w', encoding=_ENCODING) as writer:
         writer.write(s)
 
 
@@ -50,5 +56,44 @@ def insert(file, s, position):
     raise NotImplemented()
 
 
-def read(file):
-    raise NotImplemented()
+def read(file, silent=False):
+    """ Open file for read mode, read string from it, and close.
+
+    Read string from a file with the assumption that it is encoded in ``utf-8``.
+
+    If the file not exists, raise FileNotFoundError, or get empty string as return value
+    if silent parameter set to True.
+
+    Parameters
+    ----------
+    file: str or os.PathLike
+        File to read from.
+    silent: bool, default=False
+        If set to True, read empty string when file not exists.
+        If set to False, raise FileNotFoundError when file not exists.
+
+    Returns
+    -------
+    str
+        Read string.
+
+    Raises
+    ------
+    FileNotFoundError
+        File is not exist and silent parameter set to False.
+    PermissionError
+        Could not open or create file due to permission problem.
+    IsADirectoryError
+        A value specified to file parameter was a directory.
+    TypeError
+        Illegal type of parameter specified.
+    """
+    file = Path(file)
+    if not file.exists():
+        if silent:
+            return ''
+        else:
+            raise FileNotFoundError(str(file))
+
+    with open(file, encoding=_ENCODING) as reader:
+        return reader.read()
